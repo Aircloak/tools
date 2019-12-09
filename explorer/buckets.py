baed740b38ffc69617c9f308ad04b78e1838ff22
Daniel Lennon <dan@Daniels-MacBook-Pro.local>
2019-12-09 15:05:23 +0100

    add extra logging on bucket estimator

--- explorer/buckets.py
+++ explorer/buckets.py
@@ -7,7 +7,7 @@ MIN_BUCKET_COUNT = 20
 
 '''
 A note about terminology:
-Bucket size: The range of values contained in a bucket
+Bucket size: The size of the range of values contained in a bucket
 Bucket count: The number of values contained in a bucket
 Bucket number: The number of buckets contained in a range
 
@@ -25,44 +25,47 @@ class Buckets():
 
     def estimate_bucket_size(self, value_range: float, value_count: int,
                              num_buckets=MAX_BUCKETS, min_bucket_count=MIN_BUCKET_COUNT) -> int:
-        '''Estimate a suitable bucket size based on desired precision and size restrictions. 
+        '''Estimate a suitable bucket size based on desired precision and size restrictions.
 
-        :param value_range: The size of the value range to be bucketed. 
-        :param value_count: The number of values contained in the dataset. 
-        :param num_buckets: The desired number of buckets for sufficient precision / resolution. 
+        :param value_range: The size of the value range to be bucketed.
+        :param value_count: The number of values contained in the dataset.
+        :param num_buckets: The desired number of buckets for sufficient precision / resolution.
         :param min_bucket_count: The lowest number of values desired in each bucket
         :returns: A suitable bucket size.count
 
         For example, the dataset contains 10_000 values in the range 2042 -> 5683.
-        The value_range is 5683 - 2042 = 3641 
-        If num_buckets is 100, the average bucket size is 36.4 for an estimated bucket count 
-        of 100. 
-        At the min_bucket_count of 20 we would have 10_000 / 20 = 500 buckets of size 3641 / 500 = 7.2. 
-        So we would like a bucket size of at least 7.2 and we are targeting 36.4 for sufficient precision. 
-        In this range there are two suitable bucket sizes: 10 and 20. 
+        The value_range is 5683 - 2042 = 3641
+        If num_buckets is 100, the average bucket size is 36.4 for an estimated bucket count
+        of 100.
+        At the min_bucket_count of 20 we would have 10_000 / 20 = 500 buckets of size 3641 / 500 = 7.2.
+        So we would like a bucket size of at least 7.2 and we are targeting 36.4 for sufficient precision.
+        In this range there are two suitable bucket sizes: 10 and 20.
         >>> Buckets().estimate_bucket_size(5683 - 2042, 10_000)
         10
 
-        Note: 
-            - The returned size may not meet both of the desired criteria. 
-            - The min_bucket_count takes priority. 
+        Note:
+            - The returned size may not meet both of the desired criteria.
+            - The min_bucket_count takes priority.
         '''
         logging.debug(
-            f'''Estimating bucket size for: 
-                    range {value_range}, 
-                    count {value_count}, 
-                    num buckets {num_buckets}, 
+            f'''Estimating bucket size for:
+                    range {value_range},
+                    count {value_count},
+                    num buckets {num_buckets},
                     min bucket count {min_bucket_count}''')
         # Estimate lower and upper bounds for the bucket size
         precision_bound = value_range / num_buckets
         size_bound = value_range / (value_count / min_bucket_count)
+
+        logging.debug(
+            f'Precision bound: {precision_bound}, Size bound: {size_bound}')
 
         bs_candidates = self.buckets_in_range(size_bound, precision_bound)
         logging.debug(f'Options are: {bs_candidates}')
 
         if len(bs_candidates) == 0:
             # No bucket sizes within the range, prioritise the size bound
-            result = self._next_after(size_bound)
+            result = self.next_after(size_bound)
         else:
             # Otherwise choose the largest bucket size within the range
             result = max(bs_candidates)
@@ -70,8 +73,8 @@ class Buckets():
         logging.debug(f'Returning bucket size {result}')
 
         return result
-        # bs_candidate_lower = self._next_after(lower)
-        # bs_candidate_upper = self._first_before(upper)
+        # bs_candidate_lower = self.next_after(lower)
+        # bs_candidate_upper = self.first_before(upper)
         # if bs_candidate_lower == bs_candidate_upper:
         #     # There is only one bucket size that falls within the desired range
         #     return bs_candidate_lower
@@ -91,10 +94,10 @@ class Buckets():
         #             f'Unable to estimate bucket size for range {lower} -> {upper}')
         #         return 0
 
-    def _next_after(self, val):
+    def next_after(self, val):
         return next(v for v in self.buckets if v > val)
 
-    def _first_before(self, val):
+    def first_before(self, val):
         return next(v for v in reversed(self.buckets) if v < val)
 
     def buckets_smaller_than(self, val):
